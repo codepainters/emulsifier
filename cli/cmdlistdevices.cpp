8a4eee1b16e1e0eb6150f923a528fd6a6a77e38f
Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
2014-03-01 19:18:09 +0100

    Added device list printout

--- cli/cmdlistdevices.cpp
+++ cli/cmdlistdevices.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <iostream>
+#include <iomanip>
 
 #include "cmdlistdevices.h"
 
@@ -11,13 +12,31 @@ CmdListDevices::CmdListDevices(MidiIO* midi) :
 
 void CmdListDevices::execute()
 {
+    std::cout << "Devices:" << std::endl;
+
     connect(inquiry, SIGNAL(deviceInquiryCompleted(QList<MidiDevice>)), this, SLOT(deviceInquiryCompleted(QList<MidiDevice>)));
-    inquiry->startDeviceInquiry(5000);
+    inquiry->startDeviceInquiry();
 }
 
 void CmdListDevices::deviceInquiryCompleted(const QList<MidiDevice>& devices)
 {
-    std::cout << "Devices:" << std::endl;
+
+    MidiDevice device;
+    unsigned int idx = 1;
+    foreach(device, devices) {
+        std::cout << " " << std::dec << std::setw(2) << idx++ << " ";
+
+        // set integers to be printed as 0xnn, 0 padded
+        std::cout << std::showbase << std::internal << std::setfill('0') << std::hex;
+
+        std::cout << std::setw(6) << device.manufacturerId << " ";
+        std::cout << std::setw(6) << device.familyCode << " ";
+        std::cout << std::setw(6) << device.deviceMemberCode << " ";
+        std::cout << std::endl;
+
+    }
+
+    std::cout << std::endl;
 
     QApplication::exit();
 }
--- cli/main.cpp
+++ cli/main.cpp
@@ -12,15 +12,17 @@ int main(int argc, char *argv[])
     QStringList ports = midi.availableInputs();
     std::cout << "MIDI INs:" << std::endl;
     foreach(QString port, ports)
-        std::cout << port.toUtf8().constData() << std::endl;
+        std::cout << " " << port.toUtf8().constData() << std::endl;
+    std::cout << std::endl;
 
     ports = midi.availableOutputs();
     std::cout << "MIDI OUTs:" << std::endl;
     foreach(QString port, ports)
-        std::cout << port.toUtf8().constData() << std::endl;
+        std::cout << " " << port.toUtf8().constData() << std::endl;
+    std::cout << std::endl;
 
-    midi.selectInput(0);
-    midi.selectOutput(0);
+    midi.selectInput(2);
+    midi.selectOutput(6);
 
     CmdListDevices cmd(&midi);
     cmd.execute();
